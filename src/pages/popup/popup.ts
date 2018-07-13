b4f0db7a10078ae22848ecf9a24e75335393a8c7
BeauitfullXiaoJian <xiaojiannangkang@126.com>
2018-07-13 16:33:41 +0800

    add switch

--- src/pages/popup/popup.ts
+++ src/pages/popup/popup.ts
@@ -8,9 +8,16 @@ class PopupController extends PageServiceController {
     onInit() {
         $('[target="default"]').click(() => {
             $('#default').show();
+            $('.popup-default').show();
         });
         $('[target="full"]').click(() => {
-            $('#full').show();
+            $('.popup-full').show();
+        });
+        $('[target="sheet"]').click(() => {
+            $('.popup-sheet').show();
+        });
+        $('.popup-sheet').click(() => {
+            $('.popup-sheet').hide();
         });
         $('[target="close"]').click(() => {
             $('.popup').hide();
