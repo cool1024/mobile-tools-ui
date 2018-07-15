90f1a866f95c70b108d4cd37062be0a54d0ba9ca
BeauitfullXiaoJian <xiaojiannangkang@126.com>
2018-07-15 16:45:31 +0800

    update readme

--- README.md
+++ README.md
@@ -16,4 +16,8 @@ A tool ui for mobile.
 `npm run build-prod`
 
 ## example
-A example for cordova,can run as an android or ios app
+A example for cordova,can run as an android or ios app.
+
+
+## warning
+This project will not update a long time...(no time)
--- src/pages/tab/tab.ts
+++ src/pages/tab/tab.ts
@@ -6,7 +6,16 @@ declare const $: any;
 class TabController extends PageServiceController {
 
     onInit() {
-
+        $('.tab-item').click($event => {
+            const items: HTMLDivElement[] = $('.tab-item').toArray();
+            items.forEach(div => {
+                div.classList.remove('active');
+            });
+            const div: HTMLDivElement = $event.currentTarget;
+            if (!div.classList.contains('active')) {
+                div.className += ' active';
+            }
+        });
     }
 
     fillView() {
